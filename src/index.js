b9d0636c45385346e172a6f5b7defdcae6843371
Richard <35225988+RichardMarkRoss@users.noreply.github.com>
2023-08-14 15:18:07 +0200

    provider issue

--- src/index.js
+++ src/index.js
@@ -3,17 +3,27 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './Redux/store'; // Import your Redux store
 import './index.css';
-import App from './App';
+import {App} from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
   <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
-);
+  </Provider>
+)
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   </Provider>,
+//   document.getElementById('root')
+// );
 
 reportWebVitals();
